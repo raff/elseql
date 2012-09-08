d5e192c928bdf183076e7a22f5c3c0da8b9c6c7d
Raffaele Sena <raff367@gmail.com>
2012-09-08 14:35:04 -0700

    added command line interpreter (cmd or cmd2)

--- elseql/elseql.py
+++ elseql/elseql.py
@@ -1,15 +1,118 @@
 #!/usr/bin/env python
+# Copyright (c) 2012 Raffaele Sena https://github.com/raff
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish, dis-
+# tribute, sublicense, and/or sell copies of the Software, and to permit
+# persons to whom the Software is furnished to do so, subject to the fol-
+# lowing conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABIL-
+# ITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
+# SHALL THE AUTHOR BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+
+#
+# A SQL-like command line tool to query ElasticSearch
+#
+
+if __name__ == '__main__':
+    import sys
+    if '--cmd2' in sys.argv:
+        _CMD2_REQUIRED = True
+        sys.argv.remove('--cmd2')
+    else:
+        _CMD2_REQUIRED = False
+
+try:
+    import cmd2 as cmd
+except ImportError:
+    if _CMD2_REQUIRED:
+        print ""
+        print "cmd2 is not installed: please install and try again"
+        print ""
+        raise
+    else:
+        import cmd
+
+import shlex
+import pprint
+import traceback
 
 from parser import ElseParser, ElseParserException
 from search import ElseSearch, DEFAULT_PORT
-import rawes
-import pprint
 
 class DebugPrinter:
     def write(self, s):
         print s
 
-def main(args):
+class ElseShell(cmd.Cmd):
+
+    prompt = "elseql> "
+
+    def __init__(self, port, debug):
+        cmd.Cmd.__init__(self)
+        self.search = ElseSearch(port, debug)
+
+    def getargs(self, line):
+        return shlex.split(str(line.decode('string-escape')))
+
+    def do_select(self, line):
+        self.search.search('select ' + line)
+
+    def do_EOF(self, line):
+        "Exit shell"
+        return True
+
+    def do_shell(self, line):
+        "Shell"
+        os.system(line)
+
+    #
+    # override cmd
+    #
+
+    def emptyline(self):
+        pass
+
+    def onecmd(self, s):
+        print "%s/%s" % (s, type(s))
+        try:
+            return cmd.Cmd.onecmd(self, s)
+        except:
+            traceback.print_exc()
+            return False
+
+    def default(self, line):
+        line = line.strip()
+        if line and line[0] in ['#', ';']:
+            return False
+        else:
+            return cmd.Cmd.default(self, line)
+
+    def completedefault(self, test, line, beginidx, endidx):
+        list=[]
+
+        #for t in self.tables:
+        #    if t.startswith(test):
+        #        list.append(t)
+
+        return list
+
+    def postloop(self):
+        print "Goodbye!"
+
+def run_command():
+    import sys
+
+    args = sys.argv
     progname = args.pop(0)
     debug = False
 
@@ -34,15 +137,7 @@ def main(args):
         else:
             break
 
-    query = " ".join(args)
-
-    search = ElseSearch(port, debug)
-    search.search(query)
-    return 0
-
-def run_command():
-    import sys
-    main(sys.argv)
+    ElseShell(port, debug).cmdloop()
 
 if __name__ == "__main__":
     run_command()
